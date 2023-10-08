4bc43feb64f330a83e035562a8d6510756ea2813
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2023-10-07 20:50:54 -0700

    addedbetter audiio

--- JoyBoysPong/Assets/Scrpts/Balls/BouncySurface.cs
+++ JoyBoysPong/Assets/Scrpts/Balls/BouncySurface.cs
@@ -7,7 +7,6 @@ public class BouncySurface : MonoBehaviour
     public float bounceStrength;
     public AudioSource pongBounce;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +26,7 @@ public class BouncySurface : MonoBehaviour
             Vector2 normal = collision.GetContact(0).normal;
             ball.AddForce(-normal * bounceStrength);
             pongBounce.Play();
+           
         }
     }
 }
